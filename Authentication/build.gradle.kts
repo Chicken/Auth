ef377f8a5f9c4eef5f117733dd724f88de182dc1
Antti Ellilä <antti@antti.codes>
2024-04-04 13:18:03 +0300

    chore: bump versions

--- Authentication/build.gradle.kts
+++ Authentication/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "codes.antti.auth"
-version = "0.2.0"
+version = "0.3.0"
 
 repositories {
     mavenCentral()
--- Authorization/build.gradle.kts
+++ Authorization/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "codes.antti.auth"
-version = "0.2.0"
+version = "0.2.1"
 
 repositories {
     mavenCentral()
