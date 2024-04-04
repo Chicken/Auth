18f15f82cfdec32e37d7e07df1993418f28af777
Antti Ellilä <antti@antti.codes>
2024-04-04 13:13:33 +0300

    chore: remove hangar publish tasks

--- Authorization/build.gradle.kts
+++ Authorization/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     id ("com.github.johnrengelman.shadow") version "8.1.1"
     id ("com.modrinth.minotaur") version "2.+"
-    id ("io.papermc.hangar-publish-plugin") version "0.0.5"
 }
 
 group = "codes.antti.auth"
@@ -81,38 +80,10 @@ modrinth {
             "1.17", "1.17.1",
             "1.18", "1.18.1", "1.18.2",
             "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4",
-            "1.20", "1.20.1"
+            "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4",
     )
-}
-
-hangarPublish {
-    publications.register("plugin") {
-        version.set(project.version as String)
-        namespace("Antti", "Authorization")
-        channel.set("Alpha")
-        changelog.set("View the changelog at [GitHub releases](https://github.com/Chicken/Auth/releases/tag/authorization-v${project.version})")
-        apiKey.set(System.getenv("HANGAR_TOKEN"))
-        platforms {
-            register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
-                // url.set("https://github.com/Chicken/Auth/releases/download/authorization-v${project.version}/Authorization-${project.version}.jar")
-                jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-                dependencies.url("LuckPerms", "https://luckperms.net/")
-                platformVersions.set(listOf(
-                        "1.13.2",
-                        "1.14", "1.14.1", "1.14.2", "1.14.3", "1.14.4",
-                        "1.15", "1.15.1", "1.15.2",
-                        "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.16.5",
-                        "1.17", "1.17.1",
-                        "1.18", "1.18.1", "1.18.2",
-                        "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4",
-                        "1.20", "1.20.1"
-                ))
-            }
-        }
-    }
 }
 
 tasks.register("publish") {
     dependsOn("modrinth")
-    dependsOn("publishPluginPublicationToHangar")
 }
