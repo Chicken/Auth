c6548bc7c9f9c2562cc68b34e8e06dbe2edc1ff6
Eatham532 <eatham532@gmail.com>
2024-08-15 10:46:08 +1000

    Different Chat viewing modes
    Signed-off-by: Eatham532 <eatham532@gmail.com>

--- BlueMap/Chat/build.gradle.kts
+++ BlueMap/Chat/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "codes.antti.auth.bluemap"
-version = "0.2.1"
+version = "0.3.0"
 
 repositories {
     mavenCentral()
--- BlueMap/Chat/src/main/resources/bluemap-chat.js
+++ BlueMap/Chat/src/main/resources/bluemap-chat.js
@@ -21,7 +21,7 @@ const nbps = String.fromCharCode(160);
 
 const chatSvg = `<svg viewBox="0 0 30 30"><path d="m 22.988,6.045 c 1.616,0 2.938,0.94 2.938,2.09 v 10.927 c 0,1.15 -1.322,2.09 -2.938,2.09 H 8.55 c -1.616,0 -2.938,-0.94 -2.938,-2.09 V 8.135 c 0,-1.15 1.322,-2.09 2.938,-2.09 z"/><path d="M 12.757,20.93 4.074,23.956 5.795,14.923 Z"/></svg>`;
 const expandSvg = `<svg viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M10.94 7.94a1.5 1.5 0 0 1 2.12 0l5.658 5.656a1.5 1.5 0 1 1-2.122 2.121L12 11.122l-4.596 4.596a1.5 1.5 0 1 1-2.122-2.12z"/></g></svg>`
-const closeSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 7l10 10M7 17L17 7"/></svg>`
+const closeSvg = `<svg width="128" height="128" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 7l10 10M7 17L17 7"/></svg>`
 
 void async function() {
     const auth = await getAuthStatus().catch(() => {
@@ -33,9 +33,26 @@ void async function() {
     const root = document.createElement("div");
     root.id = "chat-root";
     root.style.display = "none";
+    // Messages Popup: The div for short term messages
+    const messagesPopup = document.createElement("div");
+    messagesPopup.id = "chat-messages-popup"
+    // Chat Messages: The div for long term messages
     const chatMessages = document.createElement("div");
     chatMessages.id = "chat-messages";
+    chatMessages.classList.add("messages-div-hidden");
+
+    // Toggle Chat View Control: Button to toggle the view mode. located next to the text-box
+    const toggleChatViewControl = document.createElement("div");
+    toggleChatViewControl.id = "toggle-chat-view-control";
+    toggleChatViewControl.classList.add("svg-button")
+    toggleChatViewControl.innerHTML = expandSvg;
+
+    root.classList.add("no-messages");
+
+    root.appendChild(messagesPopup);
     root.appendChild(chatMessages);
+    root.appendChild(toggleChatViewControl);
+
     const chatInput = document.createElement("input");
     chatInput.id = "chat-input";
     chatInput.placeholder = "Enter your message...";
@@ -84,8 +101,35 @@ void async function() {
         chatIsOpen = !chatIsOpen;
     }
 
+    let chatExpanded = false;
+    function toggleExpanded() {
+        if (chatExpanded) {
+            // If viewing the div just for popups
+            chatMessages.classList.add("messages-div-hidden");
+            messagesPopup.classList.remove("messages-div-hidden");
+            toggleChatViewControl.innerHTML = expandSvg;
+
+            if (messagesPopup.children.length === 0) {
+                root.classList.add("no-messages");
+            }
+            chatExpanded = false;
+        }
+        else {
+            // If viewing the expanded version
+            chatMessages.classList.remove("messages-div-hidden");
+            messagesPopup.classList.add("messages-div-hidden");
+            toggleChatViewControl.innerHTML = closeSvg;
+            if (chatMessages.children.length !== 0) {
+                root.classList.remove("no-messages");
+            }
+            chatExpanded = true;
+        }
+    }
+
     toggleBtnControl.addEventListener("click", toggleChat);
     toggleBtnZoom.addEventListener("click", toggleChat);
+
+    toggleChatViewControl.addEventListener("click", toggleExpanded);
 
     chatInput.addEventListener("keydown", (e) => {
         e.stopPropagation();
@@ -103,12 +147,24 @@ void async function() {
         }
     });
 
+    addEventListener("keydown", (e) => {
+            if (e.key === "t") {
+                toggleChat();
+                if (!chatExpanded) {
+                    toggleExpanded();
+                }
+            }
+        }
+    )
+
     // Max message count. Check if it's set. If not, use 100
     let mmc = "{{max-message-count}}"
     // It is replaced from java side. To prevent this if statement from being replaced, a little hack is used.
     if (mmc === "{{" + "max-message-count" + "}}") {
         mmc = "100";
     }
+
+
     function addMessage(str, className) {
         const message = document.createElement("div");
         message.innerText = str;
@@ -116,9 +172,22 @@ void async function() {
             message.className = className;
         }
         chatMessages.insertBefore(message, chatMessages.firstChild);
+        let pc = message.cloneNode(true);
+        messagesPopup.insertBefore(pc, messagesPopup.firstChild);
+        setTimeout(() => {
+            pc.classList.add("fade-out");
+            setTimeout(() => {
+                messagesPopup.removeChild(pc);
+
+                if (messagesPopup.children.length === 0 && !chatExpanded) {
+                    root.classList.add("no-messages");
+                }
+            }, 500)
+        }, 5000)
         if (chatMessages.children.length > mmc.toString()) {
             chatMessages.removeChild(chatMessages.lastChild);
         }
+        root.classList.remove("no-messages");
     }
 
     // Web chat prefix. Check if it's set. If not, use [web]
